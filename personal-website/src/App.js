c10f9b11d15caff08546533af7b73101cd7925a5
AustinB12 <austinbaird.unc@gmail.com>
2019-08-17 15:29:22 -0400

    nothing

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -8,7 +8,6 @@ function App() {
       <header className="App-header">
         <h1>Hello! My name is Austin Baird, and I'm a web developer!</h1>
         <img className="App-logo" src={logo} alt="logo" />
-        <a href="https://stackexchange.com/users/13636004" /><img src="https://stackexchange.com/users/flair/13636004.png" width="208" height="58" alt="profile for Austin B on Stack Exchange, a network of free, community-driven Q&amp;A sites" title="profile for Austin B on Stack Exchange, a network of free, community-driven Q&amp;A sites" />
       </header>
     </div>
   );
