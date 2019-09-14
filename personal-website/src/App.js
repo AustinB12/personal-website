f7c2a8b0a60e17c38aa0e704db4f9599b0f67249
Austin Baird <abaird@linq.com>
2019-09-14 10:52:13 -0400

    Removing BNB

--- personal-website/src/App.js
+++ personal-website/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './DuolingoBird.svg';
-import bnb from './BadNewsBears.svg';
 import './App.css';
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
       <header className="App-header">
         <h1>Hello! My name is Austin Baird, and I'm a web developer!</h1>
         <img className="App-logo" src={logo} alt="logo" />
-        <img src={bnb} alt="bnb" />
       </header>
     </div>
   );
