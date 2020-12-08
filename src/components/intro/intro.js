3fb81a2dcc8ccbf52e490dceb3b326f7b5ce9759
Austin Baird <austinzbaird@gmail.com>
2020-12-08 17:18:54 -0500

    tik tok

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -14,6 +14,32 @@ const Intro = () => {
           work experience includes doing software development for Corning
           Incorporated, EMS Linq, and Ally Financial where I work currently.
         </p>
+        <blockquote
+          className="tiktok-embed"
+          cite="https://www.tiktok.com/@codingtuts/video/6904007360944098565"
+          data-video-id="6904007360944098565"
+          style={{ maxWidth: 605, minWidth: 325 }}
+        >
+          <section>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              title="@codingtuts"
+              href="https://www.tiktok.com/@codingtuts"
+            >
+              @codingtuts
+            </a>
+            <p>For loops in Python!</p>{" "}
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              title="♬ original sound - Austin Baird"
+              href="https://www.tiktok.com/music/original-sound-6904007495858064133"
+            >
+              ♬ original sound - Austin Baird
+            </a>
+          </section>
+        </blockquote>
       </div>
     </div>
   );
