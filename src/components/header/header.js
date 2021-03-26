0aa3f3b0872fb065e61c431f5732ffca0d7b088d
Austin B <42442591+AustinB12@users.noreply.github.com>
2021-03-26 11:06:06 -0400

    🔥 Removed Select Package for Vanilla Select

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import SideNav from "./sideNav/sideNav";
 import SideTheme from "./sideTheme/sideTheme";
-import Select from "react-select";
 
 import { ReactComponent as Burger } from "../../assets/Burger.svg";
 import { ReactComponent as Color } from "../../assets/ColorWheel.svg";
@@ -13,48 +12,25 @@ const Header = (props) => {
   const [isNavOpen, setNavOpen] = useState(false);
   const [isThemeOpen, setThemeOpen] = useState(false);
 
-  const options = [
-    { value: "dark-theme", label: "Midnight" },
-    { value: "light-theme", label: "Light" },
-    { value: "red-theme", label: "Warm" },
-    { value: "blue-theme", label: "Ocean" },
-    { value: "green-theme", label: "Garden" },
-  ];
-
-  const getLabel = (value) => {
-    switch (value) {
-      case "dark-theme":
-        return options[0].label;
-      case "light-theme":
-        return options[1].label;
-      case "red-theme":
-        return options[2].label;
-      case "blue-theme":
-        return options[3].label;
-      case "green-theme":
-        return options[4].label;
-      default:
-        return options[0].label;
-    }
-  };
-
   return (
     <div className={"headerWrapper"}>
       <nav className={"wide"}>
         <div className={"theme-wrapper"}>
           <span>Theme Selector</span>
-          <Select
+          <select
             className="theme-select"
             name={"themes"}
             id={"themeSelect"}
-            onChange={(e) => props.changeTheme(e.value)}
-            options={options}
+            onChange={(e) => props.changeTheme(e.target.value)}
             placeholder="Select a Theme..."
-            value={{
-              value: props.currentTheme,
-              label: getLabel(props.currentTheme),
-            }}
-          ></Select>
+            value={props.currentTheme}
+          >
+            <option value="dark-theme">Midnight</option>
+            <option value="light-theme">High Light</option>
+            <option value="red-theme">Tangarine Dream</option>
+            <option value="blue-theme">Wrightsville</option>
+            <option value="green-theme">Garden</option>
+          </select>
         </div>
         <ul className={"link-holder"}>
           <Link to="/">
@@ -68,6 +44,7 @@ const Header = (props) => {
           </Link>
         </ul>
       </nav>
+
       <nav className={"narrow"}>
         <div role="button" onClick={() => setThemeOpen(true)}>
           <Color id="colorWheel" />
