0fb990de310c5326d19fbc9f95a0e665f7eb162f
Austin B <42442591+AustinB12@users.noreply.github.com>
2021-05-20 14:41:46 -0400

    🎨 Moving in Self Portrait

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactComponent as Me } from "../../assets/ItsMe-Circle.svg";
 
 import "./intro.css";
 
@@ -9,6 +10,7 @@ const Intro = () => {
         <article>
           <h3>Hi, I'm Austin Baird</h3>
           <p>and I'm a web developer.</p>
+          <Me id="meSvg" />
         </article>
       </div>
     </div>
